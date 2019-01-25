fa19dc3260f243b79f1815e2424e587c360b3bf1
Lastofthefirst <47003973+Lastofthefirst@users.noreply.github.com>
2019-01-25 09:47:34 -0500

    Version 2
    Each note created generates a new title, h5 and p element. This approach allows them to be grouped and organized and increases readability.

--- main.js
+++ main.js
@@ -6,14 +6,37 @@ let title = "",
     notedDate = document.getElementById('notedDate'),
     titleNote = document.getElementById('notedTitle');
 
-
+//The below function is called each submit button press to generate a new note.
 function submitNotes(){
-  body += '\n' + document.getElementById('bodyInput').value,
-  title += '\n' + document.getElementById('titleInput').value,
-  timeStamp += '\n' + Date();
+  body = document.getElementById('bodyInput').value,
+  title = document.getElementById('titleInput').value,
+  timeStamp = '\n' + Date();
 
-  titleNote.innerText = title;
-  notedDate.innerText = timeStamp;
-  bodyNote.innerText = body;
+//Creates the a variable to create an h1, and a variable for to fill the text with the title value
+  let testTitle = document.createElement("h1");
+  let titleContent = document.createTextNode(title);
+
+  let subHeading = document.createElement("h5");
+  let noteTime = document.createTextNode(timeStamp);
+
+
+// Creates a variable to create a p and a variable of text filled with the body value.
+  let testBody = document.createElement("p");
+  let bodyContent = document.createTextNode(body);
+
+
+  let line = document.createElement("hr");
+
+// Fills the new elements with the text variables(which pull from user input)
+  testTitle.appendChild(titleContent);
+  testBody.appendChild(bodyContent);
+  subHeading.appendChild(noteTime);
+  let element = document.getElementById("testNote");
+// Adds the new filled elements as children of the testNote element
+  element.appendChild(testTitle);
+  element.appendChild(subHeading);
+  element.appendChild(testBody);
+  element.appendChild(line);
+
 }
 //console.log(sumbitNotes);
