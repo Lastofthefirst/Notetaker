3c704bcd899b244920b058e9e953f8648b54d9d8
Lastofthefirst <47003973+Lastofthefirst@users.noreply.github.com>
2019-01-30 11:42:24 -0500

    Cleaner Javascript, Delete All, Sidebar list

--- main.js
+++ main.js
@@ -2,16 +2,39 @@
 let title = "",
     body = "",
     timeStamp = "",
-    bodyNote = document.getElementById('notedBody'),
-    notedDate = document.getElementById('notedDate'),
-    titleNote = document.getElementById('notedTitle'),
-    notesCollection = JSON.parse(localStorage.getItem("notesArray")),
     element = document.getElementById("testNote");
+    list = document.getElementById("notes_list");
+    notesCollection = JSON.parse(localStorage.getItem("notesArray"));
     if (notesCollection==null) {
       notesCollection = [];
     };
-    submitNotes();
-//The below function is called each submit button press to generate a new note.
+    loadNotes();
+    titleList();
+
+// Below the array of note objects is iterated through adding html elements containing the properties of each note objects.
+function loadNotes(){
+    element.innerHTML = "";
+  for (i = 0; i < notesCollection.length; i++){
+    var htmlString = "<h1>"+notesCollection[i].title+"</h1><h5>"+notesCollection[i].timeStamp+"</h5><p>"+notesCollection[i].body+"</p><hr>";
+    var noteDiv = document.createElement('article');
+    noteDiv.innerHTML = htmlString;
+    element.appendChild(noteDiv);
+    if (notesCollection==null) {
+      notesCollection = [];
+    }
+}
+};
+
+function titleList(){
+  list.innerHTML = "<h1>List of Notes</h1>";
+  for (i = 0; i < notesCollection.length; i++){
+    let listContent = "<h2>" + notesCollection[i].title+"<h2>";
+    let listDiv = document.createElement('div');
+    listDiv.innerHTML = listContent;
+    list.appendChild(listDiv);
+  }
+}
+//The below function is called each submit button press to create an object with a title, timestamp and body, these are added to an array then set in localstorage
 function submitNotes(){
   let noteObject = {};
   noteObject.body = document.getElementById('bodyInput').value;
@@ -21,14 +44,17 @@ function submitNotes(){
   let notesCollectionSerialized = JSON.stringify(notesCollection);
   localStorage.setItem("notesArray", notesCollectionSerialized);
   notesCollection = JSON.parse(localStorage.getItem("notesArray"));
+  loadNotes();
+  titleList();
+};
+
+// Below localStorage is cleared, as well as the array of note objects and the output html element.
+function deleteNotes(){
+  localStorage.clear("notesArray");
+  notesCollection = [];
   element.innerHTML = "";
-  for (i = 0; i < notesCollection.length; i++){
-    var htmlString = "<h1>"+notesCollection[i].title+"</h1><h5>"+notesCollection[i].timeStamp+"</h5><p>"+notesCollection[i].body+"</p><hr>";
-    var noteDiv = document.createElement('div');
-    noteDiv.innerHTML = htmlString;
-    element.appendChild(noteDiv);
-  }
-}
+  titleList();
+};
 
 
 
@@ -37,18 +63,12 @@ function submitNotes(){
 //Creates the a variable to create an h1, and a variable for to fill the text with the title value
   let testTitle = document.createElement("h1");
   let titleContent = document.createTextNode(title);
-
   let subHeading = document.createElement("h5");
   let noteTime = document.createTextNode(timeStamp);
-
-
 // Creates a variable to create a p and a variable of text filled with the body value.
   let testBody = document.createElement("p");
   let bodyContent = document.createTextNode(body);
-
-
   let line = document.createElement("hr");
-
 // Fills the new elements with the text variables(which pull from user input)
   testTitle.appendChild(titleContent);
   testBody.appendChild(bodyContent);
