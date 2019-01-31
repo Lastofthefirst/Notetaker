4f0a23d5051abff5ea1ac1e48dbf551cd13a4293
Erik WIlder <erikkurtu@gmail.com>
2019-01-31 11:30:11 -0500

    delete individual notes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let title = "",
     body = "",
     timeStamp = "",
+    uid = "",
     element = document.getElementById("testNote");
     list = document.getElementById("notes_list");
     notesCollection = JSON.parse(localStorage.getItem("notesArray"));
@@ -13,16 +14,35 @@ let title = "",
 
 // Below the array of note objects is iterated through adding html elements containing the properties of each note objects.
 function loadNotes(){
-    element.innerHTML = "";
+  element.innerHTML = "";
   for (i = 0; i < notesCollection.length; i++){
-    var htmlString = "<h1>"+notesCollection[i].title+"</h1><h5>"+notesCollection[i].timeStamp+"</h5><p>"+notesCollection[i].body+"</p><hr>";
+    var htmlString = "<h1><a href='#' onclick='deleteMe(this);' style='float:right; margin-right:10px;' data-uid='"+notesCollection[i].uid+"'>X</a>"+notesCollection[i].title+"</h1><h5>"+notesCollection[i].timeStamp+"</h5><p>"+notesCollection[i].body+"</p><hr>";
     var noteDiv = document.createElement('article');
     noteDiv.innerHTML = htmlString;
     element.appendChild(noteDiv);
     if (notesCollection==null) {
       notesCollection = [];
     }
-}
+  }
+};
+
+function deleteMe(thisthis){
+  thisthis.parentNode.parentNode.parentNode.removeChild(thisthis.parentNode.parentNode);
+  var thisUid = thisthis.getAttribute("data-uid");
+  notesCollection = notesCollection.filter(function( obj ) {
+    return obj.uid !== thisUid;
+  });
+  let notesCollectionSerialized = JSON.stringify(notesCollection);
+  localStorage.setItem("notesArray", notesCollectionSerialized);
+  loadNotes();
+  titleList();
+  // 1. find parent, delete parent from DOM
+  // 2. find object in array
+  // 3. delete object from array
+  // 4. save array to localstorage
+  // 5. Reload notes & title list
+  // loadNotes();
+  // titleList();
 };
 
 function titleList(){
@@ -34,12 +54,19 @@ function titleList(){
     list.appendChild(listDiv);
   }
 }
+
+function createUid() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
 //The below function is called each submit button press to create an object with a title, timestamp and body, these are added to an array then set in localstorage
 function submitNotes(){
   let noteObject = {};
   noteObject.body = document.getElementById('bodyInput').value;
   noteObject.title = document.getElementById('titleInput').value;
   noteObject.timeStamp = '\n' + Date();
+  noteObject.uid = createUid();
   notesCollection.push(noteObject);
   let notesCollectionSerialized = JSON.stringify(notesCollection);
   localStorage.setItem("notesArray", notesCollectionSerialized);
