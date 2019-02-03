9cd95eb2315f8925f838b753874809ffb00c0e43
Lastofthefirst <47003973+Lastofthefirst@users.noreply.github.com>
2019-02-03 14:20:35 -0500

    Edit Note, Cancel,

--- main.js
+++ main.js
@@ -16,35 +16,53 @@ let title = "",
 function loadNotes(){
   element.innerHTML = "";
   for (i = 0; i < notesCollection.length; i++){
-    var htmlString = "<h1><a href='#' onclick='deleteMe(this);' style='float:right; margin-right:10px;' data-uid='"+notesCollection[i].uid+"'>X</a>"+notesCollection[i].title+"</h1><h5>"+notesCollection[i].timeStamp+"</h5><p>"+notesCollection[i].body+"</p><hr>";
-    var noteDiv = document.createElement('article');
-    noteDiv.innerHTML = htmlString;
-    element.appendChild(noteDiv);
-    if (notesCollection==null) {
-      notesCollection = [];
+      var htmlString = "<div><button href='#' onclick='deleteMe(this);' style='float:right; margin-right:10px;' data-uid='"+notesCollection[i].uid+"'>X</button><button href='#' onclick='editMe(this);' style='float:right; margin-right:10px;' >Edit</button><h1>"+notesCollection[i].title+"</h1><h5>"+notesCollection[i].timeStamp+"</h5><p>"+notesCollection[i].body+"</p></div><hr>";
+      var noteDiv = document.createElement('article');
+      noteDiv.innerHTML = htmlString;
+      element.appendChild(noteDiv);
+      if (notesCollection==null) {
+        notesCollection = [];
     }
   }
 };
 
+function editMe(thisthis){
+    // define the html string
+    let editString = "<div><h1><input type='text' value='"+thisthis.parentNode.children[2].innerText+"'></h1><input type='text' value='"+thisthis.parentNode.children[4].innerText+"'><button style='float:right; margin-right:10px;' onclick='loadNotes()'>Cancel</button><button href='' onclick='saveChanges(this)' style='float:right; margin-right:10px;'>Save</button></div><hr>";
+  // find the parent replace the parent with an html string.
+    thisthis.parentNode.parentNode.innerHTML = editString;
+
+}
+
+function saveChanges(thisHere){
+  // pull the text from the newly generated inputfields (how do i navigate to them, must be through the dom?)
+  let newBody = thisHere.parentNode.children[1].value;
+  let newTitle = thisHere.parentNode.children[0].children[0].value;
+  thisHere.parentNode.parentNode.innerHTML = newTitle + newBody;
+
+  // Set the .title and .body properties of the particular note object to the new input's values.
+
+};
+// Deletes a specific note from the array, localStorage and the display.
 function deleteMe(thisthis){
+    // 1. find parent of the button, delete parent from DOM
   thisthis.parentNode.parentNode.parentNode.removeChild(thisthis.parentNode.parentNode);
+    // 2. find object in array
   var thisUid = thisthis.getAttribute("data-uid");
+    // 3. delete object from array
   notesCollection = notesCollection.filter(function( obj ) {
     return obj.uid !== thisUid;
   });
+    // 4. save array to localstorage
   let notesCollectionSerialized = JSON.stringify(notesCollection);
   localStorage.setItem("notesArray", notesCollectionSerialized);
+    // 5. Reload notes & title list
   loadNotes();
   titleList();
-  // 1. find parent, delete parent from DOM
-  // 2. find object in array
-  // 3. delete object from array
-  // 4. save array to localstorage
-  // 5. Reload notes & title list
-  // loadNotes();
-  // titleList();
+
 };
 
+//Adds an h2 with the title of the new note upon creation.
 function titleList(){
   list.innerHTML = "<h1>List of Notes</h1>";
   for (i = 0; i < notesCollection.length; i++){
@@ -55,6 +73,7 @@ function titleList(){
   }
 }
 
+// This function returns a unique identifier when called.
 function createUid() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
